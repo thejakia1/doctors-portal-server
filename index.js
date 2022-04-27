77cb05c454f8da3de680d4718506f1af4fab8775
thejakia1 <thejakia1@gmail.com>
2022-04-28 05:07:13 +0600

    env cors added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello Doctors Portal!')
