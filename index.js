6cbb054445d627a12d66613139d37bd4c7f69e44
thejakia1 <thejakia1@gmail.com>
2022-04-28 19:06:38 +0600

    db connected

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello Doctors Portal!')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmx8p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("database connected successfully");
+  } finally {
+    //await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello Doctors Portal!");
+});
 
 app.listen(port, () => {
-  console.log(`listening on ${port}`)
-})
+  console.log(`listening on ${port}`);
+});
